439a340998b46076c508d405bf99b62785dee879
Max Lin <max.lin@gmx.com>
2012-06-13 15:01:24 +0800

    added license header and COPYING

--- test/test.h
+++ test/test.h
@@ -1,3 +1,26 @@
+/**************************************************************************
+ *                                                                        *
+ *  Copyright (C) 2012 by Max Lin <max.lin@gmx.com>                       *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation; either version 2 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with this program; see the file COPYING. If not, write to the   *
+ *  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,      *
+ *  Boston, MA 02110-1301, USA.                                           *
+ **************************************************************************/
+
+#ifndef __TEST_H
+#define __TEST_H
+
 #include <QObject>
 
 class Test : public QObject
@@ -9,3 +32,5 @@ public Q_SLOTS:
     void gotDeviceChanged();
     void gotKillswitchChanged();
 };
+
+#endif
